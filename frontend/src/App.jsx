da29a2ec463d9a9c21b3216844b5725c152de019
Harsh Patel <harshpatel15505@gmail.com>
2026-02-01 12:52:47 +0530

    Refactor API endpoint URL in reviewCode function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,8 +14,9 @@ function App() {
   useEffect(() => {
     prism.highlightAll();
   })
+  const url = "https://ai-code-reviewer-backend-s3hg.onrender.com";
   async function reviewCode(){
-    const response = await axios.post('https://ai-code-reviewer-backend-s3hg.onrender.com',{code})
+    const response = await axios.post(`${url}/ai/get-review`,{code})
     // backend returns { response: ... }
     setReview(response.data.response);
     console.log(response.data);
